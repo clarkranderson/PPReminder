109901d7f2d91e4e2132a4dab59a73b7bbcd6fc5
Duane Teeters <dteeters@paypal.com>
2012-02-10 15:44:26 -0600

    initial add

---
+++ PPReminder/PPReminder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PPReminder
+//
+//  Created by Duane Teeters on 2/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ PPReminder/PPReminder/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  PPReminder
+//
+//  Created by Duane Teeters on 2/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
